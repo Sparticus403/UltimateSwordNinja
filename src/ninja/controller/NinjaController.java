59252d01cc8f94e47af20cf46cec1a1b46e47fe7
GearShift24 <gearshift24@gmail.com>
2017-01-09 09:39:42 -0700

    updated controller to link with frame, need toodds frame!

--- src/ninja/controller/NinjaController.java
+++ src/ninja/controller/NinjaController.java
@@ -1,9 +1,35 @@
 package ninja.controller;
 
+/**
+ * Joe Wrigley
+ * 
+ * added imports, created ninja controller and made frame appear from todds frame!
+ */
+import javax.swing.JOptionPane;
+import ninja.model.*;
+import ninja.view.NinjaFrame;
+import java.util.ArrayList;
+
 public class NinjaController {
 
+	private NinjaFrame ninjaFrame;
+	
+	
+	
+	public NinjaController()
+	{
+		ninjaFrame = new NinjaFrame(this);
+	}
+	
 	public void start()
 	{
-		
+		JOptionPane.showMessageDialog(ninjaFrame, "welcome to SwordNinja!!!!");
 	}
+	
+	
+	public NinjaFrame getNinjaFrame()
+	{
+		return ninjaFrame;
+	}
+	
 }
